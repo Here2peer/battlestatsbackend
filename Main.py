4eb56de280bfa6782662d29a25ee1a08bde0c540
RvdM <rvdm1152@gmail.com>
2018-05-24 14:21:04 +0200

    save commit 1

--- Main.py
+++ Main.py
@@ -1,24 +1,31 @@
-from flask import Flask, session, request, redirect, url_for, g
+from flask import Flask, session, redirect, url_for, jsonify
 from flask_openid import OpenID
-import json
+import json, requests, os
 
 app = Flask(__name__)
+d = {}
+with open('ApiKey.txt', 'r') as f:
+    for line in f:
+        (key, value) = line.split(":")
+        d[key] = value
 app.config.update(
-    SECRET_KEY = 'ApiKey.txt',
-    DEBUG = True
+    SECRET_KEY = os.urandom(24),
+    DEBUG = True,
+    TESTING = True
 )
 openID = OpenID(app)
+def getInfo():
+    data = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s&format=json" % (d['steam'].strip(), session['ID']))
+    #test = json.loads(data.text) <-- convert to python shit called dict
+    return data.text
 
 @app.route("/")
 def home():
-    if g.user:
-        return getInfo(), 200
+    if 'ID' in session:
+        #return '>%s<' % d['steam']#.strip()
+        #return "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s&format=json" % (d['steam'], session['ID']), 200
+        return 'Logged in as: %s' % json.loads(getInfo())['response']['players'][0]['personaname'], 200
     return 'Not logged in', 200
-
-def getInfo():
-    request.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%d" % (SECRET_KEY, g.user))
-    data = json.loads(request.data)
-    return data['respone'] or {}
 
 @app.route('/login')
 @openID.loginhandler
@@ -27,10 +34,12 @@ def login():
 
 @openID.after_login
 def go(resp):
-    g.user = resp.identity_url
+    #app.config.update(SECRET_KEY =  resp.identity_url.split('/')[-1]) <-- plz dont remove, may com in handy later
+    app.secret_key = resp.identity_url.split('/')[-1]
+    session['ID'] = app.config['SECRET_KEY']
     return redirect(url_for('home'))
 
 @app.route("/logout")
 def logout():
-    session.pop('openid', None)
+    session.pop('ID', None)
     return redirect(url_for('home'))
