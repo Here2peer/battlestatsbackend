36b9a6cf332ed19424c42c4b4556b6276808e4e3
Rick Huizing <rickhuizing@live.nl>
2018-05-28 19:20:15 +0200

    methodized creation of champion description

--- championData/updateChampionData.py
+++ championData/updateChampionData.py
@@ -3,8 +3,10 @@
 from championData.dataParser.inireader import read_and_convert
 from championData.dataParser.jsonreader import combine_json_data
 
+
 def update_champion_data():
     read_and_convert()
     combine_json_data()
+
 
 update_champion_data()
